21c2cc58ba653afe8572062ecdcbf1c812413bd3
KaistenG <129497085+KaistenG@users.noreply.github.com>
2025-10-23 14:35:47 +0200

    Erster Commit, mit Ressourcegroupe, Storagecontainer und upload der index.html aus /website

---
+++ terraform/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">=1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "221b02a8-00e5-4b5e-82fe-e22383354732" #Zum checken meiner Azure for Students Subscription
+}
